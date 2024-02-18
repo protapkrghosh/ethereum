4c8629cc59b7b484d1e4ca4d4edb8982010be99d
Protap Ghosh <protapg518@gmail.com>
2024-02-18 22:29:31 +0600

    added footer section

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Container from '../../SectionComs/Container/Container';
 import img from '../../assets/icons/Vector.png'
 import { GoDotFill } from "react-icons/go";
@@ -12,9 +13,9 @@ const Footer = () => {
           </div>
 
           <div className='flex items-center text-[#676a8f] font-urbanist gap-x-3 relative'>
-            <a href='#' className='hover:underline'>Terms of use</a>
+            <Link to='/' className='hover:underline'>Terms of use</Link>
             <GoDotFill className='text-[11px]'/>
-            <a href='#' className='hover:underline'>Privacy notice</a>
+            <Link to='/' className='hover:underline'>Privacy notice</Link>
             <span className='text-white text-[15px] bg-[#17182a] px-2 py-[1px] border border-[#1e2037] rounded-md'>V0 29 0</span>
           </div>
         </div>
