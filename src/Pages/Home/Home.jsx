fb457c9e2d5d263c00f57fb04d3c2d5cae944b0f
Protap Ghosh <protapg518@gmail.com>
2024-02-22 03:53:45 +0600

    added banner section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import Faq from "@/SectionComs/FAQ/Faq";
 import LidoStatistics from "@/SectionComs/LidoStatistics/LidoStatistics";
-// import Rewords from "@/SectionComs/Rewords/Rewords";
+import Rewords from "@/SectionComs/Rewords/Rewords";
 
 const Home = () => {
   return (
     <div>
-      {/* <Rewords /> */}
+      <Rewords />
       <LidoStatistics />
       <Faq />
     </div>
--- src/SectionComs/Rewords/Rewords.jsx
+++ src/SectionComs/Rewords/Rewords.jsx
@@ -1,13 +1,33 @@
 import Container from "../Container/Container";
+import shadow1 from "@/assets/icons/Ellipse51.png"
+import shadow2 from "@/assets/icons/Ellipse6.png"
 
 const Rewords = () => {
   return (
-    <div className="bg-[#11121F] min-h-[450px]">
+    <div className="bg-[#11121F] min-h-[450px] overflow-hidden">
       <Container>
-        <div>
-          <div className="text-center pt-40">
+        <div className="relative">
+          <div>
+            <img src={shadow1} alt="Image" className="absolute ml-20" />
+            <img src={shadow2} alt="Image" className="absolute ml-[400px]" />
+          </div>
+
+          {/* <div style={{ backgroundImage: `url(${shadow1})` }} className="h-[600px] bg-repeat-x bg-left" /> */}
+
+          {/* <div className="absolute bottom-0">
+            <div style={{ backgroundImage: `url(${shadow1})` }} className="h-[500px] bg-no-repeat bg-right " />
+          </div> */}
+
+          <div className="text-center absolute top-[10rem] left-[12.5rem]">
             <p className="text-white text-[30px] font-medium mb-2 leading-9">Received a reward from us? <br /> Congratulations</p>
-            <p className="text-[#8877c3] md:w-[50%] xl:w-[42%] 2xl:w-[34%] mx-auto">If you’ve received the Ethereum Airdrop in the ongoing event you’re just a step away from calming your 10 ETH rewards for free</p>
+            <p className="text-[#8877c3] md:w-[50%] xl:w-[61%] 2xl:w-[34%] mx-auto">If you’ve received the Ethereum Airdrop in the ongoing event you’re just a step away from calming your 10 ETH rewards for free</p>
+          </div>
+        </div>
+
+        {/* Card */}
+        <div className="border-[#1f213a]">
+          <div>
+
           </div>
         </div>
       </Container>
