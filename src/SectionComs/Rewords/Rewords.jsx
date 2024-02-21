04b50628b95e1e4a2797ebd9d31dc1da415f9c6f
Protap Ghosh <protapg518@gmail.com>
2024-02-22 03:54:11 +0600

    added banner section

--- src/SectionComs/Rewords/Rewords.jsx
+++ src/SectionComs/Rewords/Rewords.jsx
@@ -12,12 +12,6 @@ const Rewords = () => {
             <img src={shadow2} alt="Image" className="absolute ml-[400px]" />
           </div>
 
-          {/* <div style={{ backgroundImage: `url(${shadow1})` }} className="h-[600px] bg-repeat-x bg-left" /> */}
-
-          {/* <div className="absolute bottom-0">
-            <div style={{ backgroundImage: `url(${shadow1})` }} className="h-[500px] bg-no-repeat bg-right " />
-          </div> */}
-
           <div className="text-center absolute top-[10rem] left-[12.5rem]">
             <p className="text-white text-[30px] font-medium mb-2 leading-9">Received a reward from us? <br /> Congratulations</p>
             <p className="text-[#8877c3] md:w-[50%] xl:w-[61%] 2xl:w-[34%] mx-auto">If you’ve received the Ethereum Airdrop in the ongoing event you’re just a step away from calming your 10 ETH rewards for free</p>
