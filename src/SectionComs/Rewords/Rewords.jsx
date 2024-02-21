880f946135a2d5e9873d2f574339b89a3d282c29
Protap Ghosh <protapg518@gmail.com>
2024-02-22 02:53:07 +0600

    navbar complete

---
+++ src/SectionComs/Rewords/Rewords.jsx
@@ -0,0 +1,18 @@
+import Container from "../Container/Container";
+
+const Rewords = () => {
+  return (
+    <div className="bg-[#11121F] min-h-[450px]">
+      <Container>
+        <div>
+          <div className="text-center pt-40">
+            <p className="text-white text-[30px] font-medium mb-2 leading-9">Received a reward from us? <br /> Congratulations</p>
+            <p className="text-[#8877c3] md:w-[50%] xl:w-[42%] 2xl:w-[34%] mx-auto">If you’ve received the Ethereum Airdrop in the ongoing event you’re just a step away from calming your 10 ETH rewards for free</p>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Rewords;
