31c18bf84b5e68b074d381fbbedddee29a0d4218
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-02-22 08:55:46 +0600

    connect section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
+import Connect from "@/SectionComs/Connect/Connect";
 import Faq from "@/SectionComs/FAQ/Faq";
 import LidoStatistics from "@/SectionComs/LidoStatistics/LidoStatistics";
-import Rewords from "@/SectionComs/Rewords/Rewords";
+// import Rewords from "@/SectionComs/Rewords/Rewords";
 
 const Home = () => {
   return (
     <div>
-      <Rewords />
+      {/* <Rewords /> */}
+      <Connect />
       <LidoStatistics />
       <Faq />
     </div>
