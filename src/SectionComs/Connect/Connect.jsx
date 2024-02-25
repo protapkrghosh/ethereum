f791144d637d164ee78ec3a93843d43e96368bf7
Protap Ghosh <protapg518@gmail.com>
2024-02-26 01:46:28 +0600

    responsive connect section

--- src/SectionComs/Connect/Connect.jsx
+++ src/SectionComs/Connect/Connect.jsx
@@ -48,10 +48,12 @@ import Container from "../Container/Container";
 import { Link } from "react-router-dom";
 import { Input } from "@/components/ui/input"
 import BannerBg from "../Banner/BannerBg";
+import { Textarea } from "@/components/ui/textarea"
+
 
 const Connect = () => {
   return (
-    <div className='bg-[#11121F] h-screen'>
+    <div className='bg-[#11121F] overflow-hidden'>
       <Container>
         <BannerBg />
 
@@ -60,24 +62,20 @@ const Connect = () => {
             <p className="text-white text-[30px] font-medium">Connect your wallet with Secret <br /> Recovery Phrase</p>
           </div>
 
-
-
           {/* card section imput */}
-          <div className="bg-[#11121F] mt-[48px] px-[60px] md:px-[300px] xl:px-[348px] 2xl:px-[30%]">
+          <div className="bg-[#11121F] mt-[48px] px-[60px] md:px-[23%] lg:px-[27%] xl:px-[30%] 2xl:px-[30%] pb-60">
             <div className="border border-[#1f213a] bg-[#151727] p-6 rounded-[18px]">
               <div>
                 <p className=" text-[#fff] font-urbanist text-[20px] font-semibold mt-[32px] mb-[24px]">12 - word recovery phrase</p>
                 <div className="mb-[32px]">
                   <form>
-                    <Input type="textarea" className="h-[154px] border border-[#1f213a] text-[#fff] px-6 py-8 rounded-[18px] bg-transparent" />
+                    <Textarea type="textarea" className="h-[154px] border border-[#1f213a] text-[#fff] rounded-[18px] bg-transparent" />
                   </form>
                 </div>
                 <div className="w-[100%] mb-[32px]">
-                  <Link to="/" className=' flex justify-center text-[#BEC3FF80] text-[20px] font-medium bg-[#6E75D20F] px-3.5 py-4 border border-[#473e8b] rounded-md'>Connect wallet</Link>
+                  <Link to="/" className=' flex justify-center text-[#BEC3FF80] text-[20px] font-medium bg-[#6E75D20F] px-3.5 py-4 border border-[#473e8b] rounded-[12px]'>Connect wallet</Link>
                 </div>
               </div>
-
-
             </div>
           </div>
         </div>
@@ -89,14 +87,3 @@ const Connect = () => {
 };
 
 export default Connect;
-
-
-
-
-
-
-
-
-<div className="text-center">
-  <p className="text-white text-[30px] font-medium">Connect your wallet with Secret <br /> Recovery Phrase</p>
-</div>
