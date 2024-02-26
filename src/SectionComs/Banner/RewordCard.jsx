081cd94319417bc425f896e303b6133e93e79cb8
Protap Ghosh <protapg518@gmail.com>
2024-02-26 11:50:58 +0600

    responsive

--- src/SectionComs/Banner/RewordCard.jsx
+++ src/SectionComs/Banner/RewordCard.jsx
@@ -36,10 +36,10 @@ const RewordCard = () => {
   };
 
   return (
-    <div className="bg-[#11121F] mt-[-10px] pb-6">
+    <div className="bg-[#11121F] mt-[-10px] pb-6 pt-1">
       <Container>
         {/* Card */}
-        <div className='bg-[#11121F] w-[676px] 2xl:w-[764px] mx-auto mt-[48px] pb-60"'>
+        <div className='bg-[#11121F] w-[80%] md:w-[576px] lg:w-[576px] xl:w-[676px] 2xl:w-[764px] mx-auto mt-[48px] pb-60"'>
           <div className="border border-[#1f213a] rounded-[18px]">
             <div>
               <div className="px-6 pt-6">
--- src/SectionComs/LidoStatistics/LidoStatistics.jsx
+++ src/SectionComs/LidoStatistics/LidoStatistics.jsx
@@ -2,26 +2,28 @@ import { Link } from "react-router-dom";
 
 const LidoStatistics = () => {
   return (
-    <div className="bg-[#11121F] px-[30px] md:px-[23%] lg:px-[25%] pt-2 xl:px-[28%] 2xl:px-[30%] -mt-[2px]">
-      <div className="flex justify-between items-center text-[18px] mb-3 mt-2">
-        <p className="text-white font-medium">Lido statistics</p>
-        <Link to='/' className="text-[#686b91] hover:underline">View on Ethersean</Link>
-      </div>
-
-      <div className="bg-[#131524] border border-[#1f213a] rounded-[18px]">
-        <div className="py-7 flex justify-between items-center px-6">
-          <p className="text-[18px] text-white capitalize">Total Rewords</p>
-          <div className="text-[19px]">
-            <p className="text-transparent bg-clip-text bg-gradient-to-t from-[#414453] from-10% to-[#b0b8da] to-90% font-semibold">9,510,644 B30 ETH</p>
-          </div>
+    <div className="bg-[#11121F] px-[30px] -mt-[10px]">
+      <div className="w-[676px] mx-auto">
+        <div className="flex justify-between items-center text-[18px] mb-3 mt-2">
+          <p className="text-white font-medium">Lido statistics</p>
+          <Link to='/' className="text-[#686b91] hover:underline">View on Ethersean</Link>
         </div>
 
-        <div className="bg-[#1f213a] h-[1px]" />
+        <div className="bg-[#131524] border border-[#1f213a] rounded-[18px]">
+          <div className="py-7 flex justify-between items-center px-6">
+            <p className="text-[18px] text-white capitalize">Total Rewords</p>
+            <div className="text-[19px]">
+              <p className="text-transparent bg-clip-text bg-gradient-to-t from-[#414453] from-10% to-[#b0b8da] to-90% font-semibold">9,510,644 B30 ETH</p>
+            </div>
+          </div>
 
-        <div className="py-7 flex justify-between items-center px-6">
-          <p className="text-[18px] text-white capitalize">Random Winners</p>
-          <div className="text-[19px]">
-            <p className="text-transparent bg-clip-text bg-gradient-to-t from-[#414453] from-10% to-[#b0b8da] to-90% font-semibold">353136</p>
+          <div className="bg-[#1f213a] h-[1px]" />
+
+          <div className="py-7 flex justify-between items-center px-6">
+            <p className="text-[18px] text-white capitalize">Random Winners</p>
+            <div className="text-[19px]">
+              <p className="text-transparent bg-clip-text bg-gradient-to-t from-[#414453] from-10% to-[#b0b8da] to-90% font-semibold">353136</p>
+            </div>
           </div>
         </div>
       </div>
