77dce05f31fd856aeb260ff16d0a392350112174
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-02-26 11:21:59 +0600

    banner section modify

--- src/SectionComs/Banner/RewordContents.jsx
+++ src/SectionComs/Banner/RewordContents.jsx
@@ -7,16 +7,17 @@ import { Input } from "@/components/ui/input"
 
 const RewordContents = () => {
   return (
-    <div className="bg-[#11121F] overflow-hidden pb-5">
+    <div className="bg-[#11121F] pb-5">
       <Container>
-        <div className="absolute top-[10.5rem] md:left-[0%] lg:left-[7%] xl:left-[16%] 2xl:left-[23rem]">
-          <div className="relative">
+        <div>
+          <div className='mt-[-200px]'>
             <div className="text-center">
-              <p className="text-white text-[30px] font-medium mb-2 leading-9">Received a reward from us? <br /> Congratulations</p>
-              <p className="text-[#8877c3] w-[95%] md:w-[50%] xl:w-[61%] 2xl:w-[61%] mx-auto">If you’ve received the Ethereum Airdrop in the ongoing event you’re just a step away from calming your 10 ETH rewards for free</p>
+              <p className="text-white text-[30px] font-medium">Received a reward from us? <br /> Congratulations</p>
+              <p className="text-[#8877c3] w-[95%] md:w-[50%] xl:w-[61%] 2xl:w-[561px] mx-auto">If you’ve received the Ethereum Airdrop in the ongoing event you’re just a step away from calming your 10 ETH rewards for free</p>
             </div>
           </div>
         </div>
+
       </Container>
     </div>
   );
