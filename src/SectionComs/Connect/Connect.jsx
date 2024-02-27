232c678905a43c40a0316c91cb9a8536e49b8574
Protap Ghosh <protapg518@gmail.com>
2024-02-27 21:24:03 +0600

    added popup

--- src/SectionComs/Connect/Connect.jsx
+++ src/SectionComs/Connect/Connect.jsx
@@ -95,8 +95,8 @@ const Connect = () => {
                     {/* <Link to="" className=' flex justify-center text-[#BEC3FF80] text-[20px] font-medium bg-[#6E75D20F] hover:bg-[#6e75d213] px-3.5 py-4 border border-[#252846ce] hover:border-[#31345e] hover:bg-[#705DE4] hover:text-white rounded-[12px] duration-300'>Connect wallet</Link> */}
 
                     <Dialog>
-                      <DialogTrigger>
-                        <button className=' flex justify-center text-[#BEC3FF80] text-[20px] font-medium bg-[#6E75D20F] hover:bg-[#6e75d213] px-3.5 py-4 border border-[#252846ce] hover:border-[#31345e] hover:bg-[#705DE4] hover:text-white rounded-[12px] duration-300'>open</button>
+                      <DialogTrigger >
+                        <Link to="" className=' flex justify-center text-[#BEC3FF80] text-[20px] font-medium bg-[#6E75D20F] hover:bg-[#6e75d213] px-3.5 py-4 border border-[#252846ce] hover:border-[#31345e] hover:bg-[#705DE4] hover:text-white rounded-[12px] duration-300'>Connect wallet</Link>
                       </DialogTrigger>
 
                       <DialogContent className="bg-gradient-to-b from-[#44266f] via-[#131524] to-[#131524]">
@@ -181,7 +181,7 @@ const Connect = () => {
             </div>
 
             <div className='flex justify-center'>
-              <p className='text-red-500 font-urbanist md:text-[20px] font-semibold -mt-52'>Connect your correct wallet</p>
+              <p className='text-red-500 font-urbanist md:text-[20px] font-semibold -mt-52 uppercase'>Connect your correct wallet</p>
             </div>
           </div>
         </div>
