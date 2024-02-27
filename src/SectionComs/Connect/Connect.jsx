74cbab822f94ca0d114adcdc5c2d64b3331f7313
Protap Ghosh <protapg518@gmail.com>
2024-02-27 22:41:14 +0600

    saf

--- src/SectionComs/Connect/Connect.jsx
+++ src/SectionComs/Connect/Connect.jsx
@@ -79,7 +79,7 @@ const Connect = () => {
           </div>
 
           {/* card section imput */}
-          <div className=''>
+          <div>
             <div className="bg-[#11121F] mt-[48px] w-[95%] md:w-[576px] lg:w-[576px] xl:w-[676px] 2xl:w-[764px] mx-auto pb-60">
               <div className="border border-[#1f213a] bg-[#151727] p-6 rounded-[18px]">
                 <div>
@@ -95,7 +95,7 @@ const Connect = () => {
                   <div className="w-full mb-[32px]">
                     {/* <Link to="" className=' flex justify-center text-[#BEC3FF80] text-[20px] font-medium bg-[#6E75D20F] hover:bg-[#6e75d213] px-3.5 py-4 border border-[#252846ce] hover:border-[#31345e] hover:bg-[#705DE4] hover:text-white rounded-[12px] duration-300'>Connect wallet</Link> */}
 
-                    <Dialog className=" ">
+                    <Dialog>
                       <DialogTrigger className="w-full">
                         <button className='w-full flex justify-center text-[#BEC3FF80] text-[20px] font-medium bg-[#6E75D20F] hover:bg-[#6e75d213] px-3.5 py-4 border border-[#252846ce] hover:border-[#31345e] hover:bg-[#705DE4] hover:text-white rounded-[12px] duration-300'>Connect wallet</button>
                       </DialogTrigger>
