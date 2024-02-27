07cdd8c10aa948de77ae9cd8ed8eedc3c0a2ca02
Protap Ghosh <protapg518@gmail.com>
2024-02-27 21:19:11 +0600

    added popup in connect section

--- src/SectionComs/Connect/Connect.jsx
+++ src/SectionComs/Connect/Connect.jsx
@@ -49,6 +49,20 @@ import { Link } from "react-router-dom";
 import { Input } from "@/components/ui/input"
 import BannerBg from "../Banner/BannerBg";
 import { Textarea } from "@/components/ui/textarea"
+import metaMask from "@/assets/wallets/Group1.png"
+import coinbase from "@/assets/wallets/Group2.png"
+import trust from "@/assets/wallets/Group3.png"
+import trezor from "@/assets/wallets/Group4.png"
+import ledger from "@/assets/wallets/Group5.png"
+import otherWallets from "@/assets/wallets/Group6.png"
+import wallet from "@/assets/wallets/wallet.png"
+import { IoMdMoon } from "react-icons/io";
+import { IoMdSunny } from "react-icons/io";
+import { GoQuestion } from "react-icons/go";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
+import { Button } from '@/components/ui/button';
+import { IoClose } from 'react-icons/io5';
 
 
 const Connect = () => {
@@ -78,14 +92,96 @@ const Connect = () => {
                     </form>
                   </div>
                   <div className="w-[100%] mb-[32px]">
-                    <Link to="" className=' flex justify-center text-[#BEC3FF80] text-[20px] font-medium bg-[#6E75D20F] hover:bg-[#6e75d213] px-3.5 py-4 border border-[#252846ce] hover:border-[#31345e] hover:bg-[#705DE4] hover:text-white rounded-[12px] duration-300'>Connect wallet</Link>
+                    {/* <Link to="" className=' flex justify-center text-[#BEC3FF80] text-[20px] font-medium bg-[#6E75D20F] hover:bg-[#6e75d213] px-3.5 py-4 border border-[#252846ce] hover:border-[#31345e] hover:bg-[#705DE4] hover:text-white rounded-[12px] duration-300'>Connect wallet</Link> */}
+
+                    <Dialog>
+                      <DialogTrigger>
+                        <button className=' flex justify-center text-[#BEC3FF80] text-[20px] font-medium bg-[#6E75D20F] hover:bg-[#6e75d213] px-3.5 py-4 border border-[#252846ce] hover:border-[#31345e] hover:bg-[#705DE4] hover:text-white rounded-[12px] duration-300'>open</button>
+                      </DialogTrigger>
+
+                      <DialogContent className="bg-gradient-to-b from-[#44266f] via-[#131524] to-[#131524]">
+                        <DialogHeader>
+                          <DialogTitle className="flex justify-between items-center mb-[30px]">
+                            {/* Tooltip question icon */}
+                            <TooltipProvider>
+                              <Tooltip>
+                                <TooltipTrigger asChild>
+                                  <GoQuestion className="text-[22px] text-[#a794be] cursor-help" />
+                                </TooltipTrigger>
+                                <TooltipContent className="bg-gradient-to-r from-[#44266f] to-[#381f5a] border-none">
+                                  <p className="text-white">Connect lots of wallet</p>
+                                </TooltipContent>
+                              </Tooltip>
+                            </TooltipProvider>
+
+                            <p className="text-[21px] text-[#ffffff] font-bold">Connect Wallet</p>
+                            <IoClose className="text-[25px] text-[#a794be]" />
+                          </DialogTitle>
+
+                          <DialogDescription>
+                            <div>
+                              <div className="space-y-4">
+                                {/* meta mask */}
+                                <div className="bg-gradient-to-r from-[#6f5ce4] to-[#8768ee] rounded-xl">
+                                  <Link to='/' className="flex justify-between items-center border border-[#6f5ce4] px-2 py-3 rounded-xl">
+                                    <div className="flex items-center">
+                                      <img src={metaMask} alt="Meta Mask" className="w-[35px] mr-3" />
+                                      <p className="text-[17px] text-[#FFFFFF]">MetaMask</p>
+                                    </div>
+                                    <Link to="/" className='text-white text-[14px] font-medium bg-[#8f73ee] px-3 py-[5px] border border-[#8f73ee] rounded-md'>Connect</Link>
+                                  </Link>
+                                </div>
+
+                                {/* Coinbase */}
+                                <Link to='/' className="flex items-center bg-gradient-to-r from-[#321b4cbe] to-[#372e6a54] border border-[#42418a50] px-2 py-3 rounded-xl">
+                                  <img src={coinbase} alt="Meta Mask" className="w-[35px] mr-3" />
+                                  <p className="text-[17px] text-[#FFFFFF]">Coinbase Wallet</p>
+                                </Link>
+
+                                {/* Trust */}
+                                <Link to='/' className="flex items-center bg-gradient-to-r from-[#1d1831] to-[#20203c77] border border-[#21233c] px-2 py-3 rounded-xl">
+                                  <img src={trust} alt="Meta Mask" className="w-[35px] mr-3" />
+                                  <p className="text-[17px] text-[#FFFFFF]">Trust Wallet</p>
+                                </Link>
+
+                                {/* Trezor */}
+                                <Link to='/' className="flex items-center bg-gradient-to-r from-[#161828] to-[#151727] border border-[#21233c] px-2 py-3 rounded-xl">
+                                  <img src={trezor} alt="Meta Mask" className="w-[35px] mr-3" />
+                                  <p className="text-[17px] text-[#FFFFFF]">Trezor</p>
+                                </Link>
+
+                                {/* Ledger */}
+                                <Link to='/' className="flex items-center bg-gradient-to-r from-[#161828] to-[#151727] border border-[#21233c] px-2 py-3 rounded-xl">
+                                  <img src={ledger} alt="Meta Mask" className="w-[35px] mr-3" />
+                                  <p className="text-[17px] text-[#FFFFFF]">Ledger</p>
+                                </Link>
+
+                                {/* Other Wallets */}
+                                <Link to='/' className="flex items-center bg-gradient-to-r from-[#161828] to-[#151727] border border-[#21233c] px-2 py-3 rounded-xl">
+                                  <img src={otherWallets} alt="Meta Mask" className="w-[35px] mr-3" />
+                                  <p className="text-[17px] text-[#FFFFFF]">Other Wallets</p>
+                                </Link>
+                              </div>
+
+                              <div className="flex justify-center mt-4">
+                                <div className="flex items-center">
+                                  <img src={wallet} alt="wallet" className="w-[20px] mr-2" />
+                                  <Link to="/" className="text-[#D3D2FF80] hover:underline">I use different wallet</Link>
+                                </div>
+                              </div>
+                            </div>
+                          </DialogDescription>
+                        </DialogHeader>
+                      </DialogContent>
+                    </Dialog>
+
                   </div>
                 </div>
               </div>
             </div>
 
             <div className='flex justify-center'>
-              <p className='text-red-500 font-urbanist md:text-[20px] font-semibold -mt-52'>connect your correct wallet</p>
+              <p className='text-red-500 font-urbanist md:text-[20px] font-semibold -mt-52'>Connect your correct wallet</p>
             </div>
           </div>
         </div>
