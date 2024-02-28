3b54f08eb0ebe4aceeed5a4b7d7c7c84680a491a
Protap Ghosh <protapg518@gmail.com>
2024-02-28 23:08:42 +0600

    completed dark light mode in this project

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import './index.css'
 import { router } from './Routes/Routes.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Toaster />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
