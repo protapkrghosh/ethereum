4f6182179671e9bef3b26b55d7cc5ac09da0b997
Protap Ghosh <protapg518@gmail.com>
2024-03-02 22:12:00 +0600

    asd

--- src/shared/NavBar/NavBar.jsx
+++ src/shared/NavBar/NavBar.jsx
@@ -281,7 +281,7 @@ const NavBar = () => {
                                         <CarouselItem className="flex justify-center text-center">
                                           <div>
                                             <h3 className="text-[21px] text-[#11121F] font-semibold">MetaMask</h3>
-                                            <img src={metaMaskLage} alt="Image" className="w-[110px] mx-auto mt-28  mb-5" />
+                                            <img src={metaMaskLage} alt="Image" className="w-[110px] mx-auto mt-28 mb-5 cursor-grab" />
                                             <p className="text-[18px] text-[#11121F] font-semibold">Connect MetaMask...</p>
                                           </div>
                                         </CarouselItem>
@@ -289,7 +289,7 @@ const NavBar = () => {
                                         <CarouselItem className="flex justify-center text-center">
                                           <div className="w-full">
                                             <h3 className="text-[21px] text-[#11121F] font-semibold">MetaMask</h3>
-                                            <img src={metaMaskLage} alt="Image" className="w-[110px] mx-auto mt-14 mb-5" />
+                                            <img src={metaMaskLage} alt="Image" className="w-[110px] mx-auto mt-14 mb-5 cursor-grab" />
                                             <p className="text-[15px] text-[#6c6d75] mb-20">Because of many requests, our ConnectWallet system is currently busy. Try again later or avoid this crowding by connecting your wallet using a 12-word recovery phrase. Thank you for your understanding.</p>
 
                                             <button className="text-white bg-[#1098FC] hover:bg-[#109afcec] p-4 rounded-[12px] w-full duration-200">Import Wallet</button>
@@ -299,8 +299,8 @@ const NavBar = () => {
                                         <CarouselItem className="flex justify-center text-center">
                                           <div className="w-full">
                                             <h3 className="text-[21px] text-[#11121F] font-semibold">Import Wallet</h3>
-                                            <img src={walletImg} alt="Image" className="w-[110px] mx-auto mt-14 mb-5" />
-                                            <p className="text-[20px] text-[#11121F] font-semibold mt-20 mb-2">Failed to authenticate</p>
+                                            <img src={walletImg} alt="Image" className="w-[110px] mx-auto mt-14 mb-5 cursor-grab" />
+                                            <p className="text-[22px] text-[#11121F] font-semibold mt-20 mb-2">Failed to authenticate</p>
                                             <p className="text-[14px] text-[#6c6d75] mb-14">connect your correct wallet</p>
 
                                             <button className="text-white bg-[#1098FC] hover:bg-[#109afcec] p-4 rounded-[12px] w-full duration-200 flex justify-center items-center">
@@ -310,7 +310,6 @@ const NavBar = () => {
                                           </div>
                                         </CarouselItem>
 
-                                        
                                       </CarouselContent>
 
                                       <CarouselPrevious className="top-[10px] left-0 bg-transparent border-none font-bold" />
