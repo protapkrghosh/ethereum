29f6a1b0129706cfa0af3b28e4fa792904899723
Protap Ghosh <protapg518@gmail.com>
2024-03-02 17:16:05 +0600

    added carousel in wallet

--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,10 @@ import './index.css'
 import { router } from './Routes/Routes.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import { MetaMaskProvider } from "@metamask/sdk-react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MetaMaskProvider
-      debug={false}
-      sdkOptions={{
-        dappMetadata: {
-          name: "Example React Dapp",
-          url: window.location.href,
-        },
-        // Other options
-      }}
-    >
-      <Toaster />
-      <RouterProvider router={router} />
-    </MetaMaskProvider>
+    <Toaster />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
